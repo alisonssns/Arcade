4b52ab6eb471255ade39ba7d9f1052788c729cbe
Alisson <alisoncracas2@gmail.com>
2026-08-20 18:19:44 -0300

    setup

--- setup.sh
+++ setup.sh
@@ -3,14 +3,14 @@
 # Para o script se algum comando falhar
 set -e
 
-echo "=== 1. Baixando e extraindo os jogos ==="
+echo "=== 1. Compilando os programas C++ ==="
+g++ controle.cpp -o Teclado -std=c++20 -pthread
+g++ main.cpp utils.cpp -o Arcade -std=c++20 -lsfml-graphics -lsfml-window -lsfml-system
+
+echo "=== 2. Baixando e extraindo os jogos ==="
 wget -O jogos_arcade.zip "https://github.com/alisonssns/Arcade/releases/download/jogos/jogos.zip"
 unzip -o jogos_arcade.zip
 rm -f jogos_arcade.zip
-
-echo "=== 2. Compilando os programas C++ ==="
-g++ controle.cpp -o Teclado -std=c++20 -pthread
-g++ main.cpp utils.cpp -o Arcade -std=c++20 -lsfml-graphics -lsfml-window -lsfml-system
 
 echo "=== 3. Ajustando permissões dos arquivos ==="
 chmod +x Teclado
